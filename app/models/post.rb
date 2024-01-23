5d9bcdf6b8a658e736fcc5a9b1ad68a54fdbd804
nlinedev <newline.dev1@gmail.com>
2024-01-23 16:07:34 -0600

    feat: Created scaffolding and model validations for post

---
+++ app/models/post.rb
@@ -0,0 +1,19 @@
+class Post < ApplicationRecord
+  validate  :validate_title_presence
+  validates :title, length: { minimum: 8 } if :title_is_present?
+
+  private
+
+  def validate_title_presence
+    errors.add(:title, "can't be blank") unless title_is_present?
+    errors.add(:content, "can't be blank") unless content_is_present?
+  end
+
+  def title_is_present?
+    title.present?
+  end
+
+  def content_is_present?
+    content.present?
+  end
+end
